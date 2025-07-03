227f51f6bbdd93b5cc02dfcf28d837d33eeea155
Phu Vo <u1153426@umail.usq.edu.au>
2025-07-03 17:22:04 +1000

    🚚 chore: Rename sidebar.tsx to nav-sidebar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
-import NavSidebar from '@/components/sidebar'
+import NavSidebar from '@/components/nav-sidebar'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
