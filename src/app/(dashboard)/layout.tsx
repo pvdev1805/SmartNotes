27080ff46a92f482be8f0b24bcc1854b11804617
Phu Vo <u1153426@umail.usq.edu.au>
2025-07-08 23:38:16 +1000

    ♻️ refactor: Refactor DashboardLayout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import Header from '@/components/header'
+import NavSidebar from '@/components/nav-sidebar'
+import Footer from '@/components/footer'
+
+export const metadata: Metadata = {
+  title: 'SmartNotes',
+  description: 'A smart note-taking application for better learning'
+}
+
+const DashboardLayout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  return (
+    <div className='min-h-screen w-full flex flex-col'>
+      <Header />
+
+      <div className='flex flex-1 overflow-hidden'>
+        <div className='flex-shrink-0'>
+          <NavSidebar />
+        </div>
+        <div className='flex flex-1 flex-col'>
+          <main className='flex-1 p-4 overflow-auto'>{children}</main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/header'
-import NavSidebar from '@/components/nav-sidebar'
-import Footer from '@/components/footer'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,21 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className='min-h-screen w-full flex flex-col'>
-          <Header />
-
-          <div className='flex flex-1 overflow-hidden'>
-            <div className='flex-shrink-0'>
-              <NavSidebar />
-            </div>
-            <div className='flex flex-1 flex-col'>
-              <main className='flex-1 p-4 overflow-auto'>{children}</main>
-              <Footer />
-            </div>
-          </div>
-        </div>
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   )
 }
